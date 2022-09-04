4d3be9b8fa958335116109c14a11dbc8c8e900a9
unknown <atul.thakur1837@gmail.com>
2022-09-04 10:58:19 +0530

    login signup with email username automatically populated with email

--- pumeet/users/adapters.py
+++ pumeet/users/adapters.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from allauth.account.adapter import DefaultAccountAdapter
+from allauth.account.utils import user_email, user_username
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 from django.conf import settings
 from django.http import HttpRequest
@@ -9,6 +10,12 @@ from django.http import HttpRequest
 class AccountAdapter(DefaultAccountAdapter):
     def is_open_for_signup(self, request: HttpRequest):
         return getattr(settings, "ACCOUNT_ALLOW_REGISTRATION", True)
+
+    def populate_username(self, request, user):
+        """Set email as username"""
+        email = user_email(user)
+        user_username(user, email)
+        user.username = email
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
